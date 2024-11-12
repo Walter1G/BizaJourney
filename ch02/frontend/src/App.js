1faeb94e58735624f02829eb68027b1eaa4345f2
Walter <2760walter@gmail.com>
2024-11-12 21:00:12 +0300

    fetch data using async/await

--- ch02/frontend/src/App.js
+++ ch02/frontend/src/App.js
@@ -3,7 +3,9 @@ import "./App.css";
 
 function App() {
   const [data, setData] = useState([]);
+  const [data1,setData1]=useState([])
 
+  // Using Fetch API
   const getUsers = () => {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
@@ -11,13 +13,32 @@ function App() {
   }
 
   useEffect(() => {
-   getUsers() 
+   getUsers()
   }, [])
+  
+  // Using Async/Await
+
+  const API_URL = "https://dummyjson.com/users";
+
+  const fetchSpeakers = async () => {
+    try {
+      const response = await fetch(API_URL);
+      const data = await response.json();
+      setData1(data.users);
+
+    } catch (error){
+      console.log("Err:", error);
+    }
+  }
+
+  useEffect(() => {
+    fetchSpeakers();
+  },[data1])
 
 
 
   return (
-    <div>
+    <>
       <h1>Welcome to Bizza Platform</h1>
       <ul>
       {
@@ -27,8 +48,20 @@ function App() {
           </li>
         ))
         }
-        </ul>
-    </div>
+      </ul>
+
+      [Text Wrapping Break]
+      <h1> Displaying Speakers information</h1>
+      <ul>
+        {data1.map(item => (
+          <li key={item.id}>
+            {item.firstName} {item.lastName}
+          </li>
+        ))}
+      </ul>
+      
+
+    </>
   );
 }
 
