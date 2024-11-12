bc0beb43fe1f6d70bb985028f9a4e9991ae763b3
Walter <2760walter@gmail.com>
2024-11-12 20:47:57 +0300

    use fetch api to fetch data

--- ch02/frontend/src/App.js
+++ ch02/frontend/src/App.js
@@ -1,10 +1,33 @@
-import logo from "./logo.svg";
+import { useState, useEffect } from 'react';
 import "./App.css";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const getUsers = () => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+    .then(data=>setData(data))
+  }
+
+  useEffect(() => {
+   getUsers() 
+  }, [])
+
+
+
   return (
     <div>
       <h1>Welcome to Bizza Platform</h1>
+      <ul>
+      {
+        data.map(speaker => (
+          <li>
+            <li key={speaker.id}>{speaker.name}, <em>{ speaker.email}</em></li>
+          </li>
+        ))
+        }
+        </ul>
     </div>
   );
 }
