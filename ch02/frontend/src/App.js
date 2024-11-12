0492d70c321953ed6cef32942ab6a5b3e7e12b52
Walter <2760walter@gmail.com>
2024-11-12 21:19:03 +0300

    use axios to fetch

--- ch02/frontend/src/App.js
+++ ch02/frontend/src/App.js
@@ -1,21 +1,25 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
+import {useQuery} from 'react-query'
+import axios from "axios";
 import "./App.css";
 
 function App() {
   const [data, setData] = useState([]);
-  const [data1,setData1]=useState([])
+  const [data1, setData1] = useState([]);
+  const [data2, setData2] = useState([]);
+  const [data3,setData3]=useState([])
 
   // Using Fetch API
   const getUsers = () => {
-    fetch('https://jsonplaceholder.typicode.com/users')
+    fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
-    .then(data=>setData(data))
-  }
+      .then((data) => setData(data));
+  };
 
   useEffect(() => {
-   getUsers()
-  }, [])
-  
+    getUsers();
+  }, []);
+
   // Using Async/Await
 
   const API_URL = "https://dummyjson.com/users";
@@ -25,42 +29,54 @@ function App() {
       const response = await fetch(API_URL);
       const data = await response.json();
       setData1(data.users);
-
-    } catch (error){
+    } catch (error) {
       console.log("Err:", error);
     }
-  }
+  };
 
   useEffect(() => {
     fetchSpeakers();
-  },[data1])
+  }, [data1]);
 
+  const loadSpeakers = () => {
+    axios.get("https://jsonplaceholder.typicode.com/users").then((response) => {
+      setData2(response.data);
+    });
+  };
 
+  useEffect(() => {
+    loadSpeakers();
+  }, []);
 
   return (
     <>
       <h1>Welcome to Bizza Platform</h1>
       <ul>
-      {
-        data.map(speaker => (
+        {data.map((speaker) => (
           <li>
-            <li key={speaker.id}>{speaker.name}, <em>{ speaker.email}</em></li>
+            <li key={speaker.id}>
+              {speaker.name}, <em>{speaker.email}</em>
+            </li>
           </li>
-        ))
-        }
+        ))}
       </ul>
-
       [Text Wrapping Break]
       <h1> Displaying Speakers information</h1>
       <ul>
-        {data1.map(item => (
+        {data1.map((item) => (
           <li key={item.id}>
             {item.firstName} {item.lastName}
           </li>
         ))}
       </ul>
-      
-
+      [Data from axios]
+      <ul>
+        {data2.map((item) => (
+          <li key={item.key}>
+            {item.name}, <em>{item.email}</em>
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
